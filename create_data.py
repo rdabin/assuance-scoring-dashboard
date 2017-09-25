81142f58218deadb062cbf0fe35700d8989db5b7
rupchap <rupchap@gmail.com>
2017-09-25 14:01:46 +0100

    use fake hrc data instead

--- create_data.py
+++
@@ -1,9 +0,0 @@
-import simulacrum as sm
-
-test = {'names': {'type': 'name'},
-        'scores': {'type': 'norm', 'mean': .5, 'sd': .2}
-        }
-
-res = sm.create(100, coltypes=test)
-
-print(res)
