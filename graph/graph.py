bf4126ed7e533be9e7c66cae0e319abce8cb8cb1
Simon Price <simonprice@Simons-MacBook-Pro.local>
2017-09-26 16:07:32 +0100

    added diagonal line (random classifier)

--- graph/graph.py
+++ graph/graph.py
@@ -5,31 +5,53 @@ import plotly.graph_objs as go
 import pandas as pd
 
 
-def get_graph(gdp_life_exp_data):
+def get_graph(roc_data):
 
-  return dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure={
-            'data': [
-                go.Scatter(
-                    x=gdp_life_exp_data[gdp_life_exp_data['continent'] == i]['gdp per capita'],
-                    y=gdp_life_exp_data[gdp_life_exp_data['continent'] == i]['life expectancy'],
-                    text=gdp_life_exp_data[gdp_life_exp_data['continent'] == i]['country'],
-                    mode='markers',
-                    opacity=0.7,
-                    marker={
-                        'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
-                    name = i
-                ) for i in gdp_life_exp_data.continent.unique()
-            ],
-            'layout': go.Layout(
-                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-                yaxis={'title': 'Life Expectancy'},
-                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                legend={'x': 0, 'y': 1},
-                hovermode='closest'
+    line = go.Scatter(
+        x=[0, 1],
+        y=[0, 1],
+        mode='lines',
+        #opacity=0.7,
+        line=dict(
+            color = ('rgb(180, 180, 180)'),
+            width = 1,
+            dash = 'dash',   # dash options include 'dash', 'dot', and 'dashdot'
+        )
+    )
+
+    curve = go.Scatter(
+        x=roc_data['FPR'],
+        y=roc_data['TPR'],
+        text='hello world',
+        mode='lines+markers',
+        opacity=0.7,
+        marker={
+            'size': 5,
+            'line': {'width': 0.5, 'color': 'white'}
+        },
+    )
+
+    #fig = go.Figure(data=[line, curve], layout=layout)
+
+
+    return html.Div([
+            dcc.Graph(
+                id='roc-curve',
+                figure={
+                    'data': [
+                        curve,
+                        line
+                    ],
+                    'layout': go.Layout(
+                        showlegend=False,
+                        xaxis={'type': 'linear', 'title': 'FPR'},
+                        yaxis={'title': 'TPR'},
+                        margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                        legend={'x': 0, 'y': 1},
+                        hovermode='closest'
+                    )
+                }
             )
-        }
+        ],
+        style={'width': '48%', 'display': 'inline-block'}
     )
